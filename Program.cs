db8e485d540250f4dbd019623e65bda73faf4a57
Nikoloz911 <nikalobjanidze014@gmail.com>
2025-03-15 17:52:01 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿
+Console.WriteLine("   ----   NuGet Package:   ----   ");
+Console.WriteLine();
+Console.WriteLine("1. Microsoft.EntityFrameworkCore (8.0.0)");
+Console.WriteLine("2. Microsoft.EntityFrameworkCore.SqlServer (8.0.0)");
+Console.WriteLine("3. Microsoft.EntityFrameworkCore.Design (8.0.0)");
+Console.WriteLine("4. Microsoft.EntityFrameworkCore.Tools (8.0.0)");
+Console.WriteLine("5. BCrypt.Net-Next (4.0.3)");
+Console.WriteLine("6. FluentValidation (11.11.0)");
+
+
+//Console.WriteLine("csproj File:");
+
+//< ItemGroup >
+
+//    < PackageReference Include = "BCrypt.Net-Next" Version = "4.0.3" />
+
+//    < PackageReference Include = "FluentValidation" Version = "11.11.0" />
+
+//    < PackageReference Include = "Microsoft.EntityFrameworkCore" Version = "8.0.0" />
+
+//    < PackageReference Include = "Microsoft.EntityFrameworkCore.SqlServer" Version = "8.0.0" />
+
+//    < PackageReference Include = "Microsoft.EntityFrameworkCore.Design" Version = "8.0.0" >
+
+//        < IncludeAssets > runtime; build; native; contentfiles; analyzers; buildtransitive </ IncludeAssets >
+
+//        < Pack > true </ Pack >
+
+//    </ PackageReference >
+
+//    < PackageReference Include = "Microsoft.EntityFrameworkCore.Tools" Version = "8.0.0" >
+
+//        < IncludeAssets > runtime; build; native; contentfiles; analyzers; buildtransitive </ IncludeAssets >
+
+//        < Pack > true </ Pack >
+
+//    </ PackageReference >
+//</ ItemGroup >    
